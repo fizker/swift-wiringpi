2f29c2c63cc29f67d7ef3d18e255fafb5d425ba7
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-05-06 01:40:08 +0200

    Restored the PinActions code so the diff have no changes

--- Sources/WiringPi/PinActions.swift
+++ Sources/WiringPi/PinActions.swift
@@ -1,18 +1,18 @@
 import CWiringPi
 
 public func digitalWrite(value: Pin.Value, to address: Pin.Address) {
-	digitalWrite(address, value.rawValue)
+	CWiringPi.digitalWrite(address, value.rawValue)
 }
 
 public func digitalRead(from address: Pin.Address) -> Pin.Value {
-	let value = digitalRead(address)
+	let value = CWiringPi.digitalRead(address)
 	return Pin.Value(rawValue: value)!
 }
 
 public func change(mode: Pin.Mode, for address: Pin.Address) {
-	pinMode(address, mode.rawValue)
+	CWiringPi.pinMode(address, mode.rawValue)
 }
 
 public func change(pull: Pin.Pull, for address: Pin.Address) {
-	pullUpDnControl(address, pull.rawValue)
+	CWiringPi.pullUpDnControl(address, pull.rawValue)
 }
